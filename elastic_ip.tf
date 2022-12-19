1625b9ca54cf4a532861b33a0de9c8091656ce8f
sai4571 <110476592+sai4571@users.noreply.github.com>
2022-12-19 16:34:53 +0530

    Update elastic_ip.tf

--- elastic_ip.tf
+++ elastic_ip.tf
@@ -1,8 +1,8 @@
-resource "aws_eip" "saieip" {
+/*resource "aws_eip" "saieip" {
   
 }
 resource "aws_eip_association" "sai_eip_assoc" {
     instance_id = aws_instance.AWSEC2Instance.id
     allocation_id = aws_eip.saieip.id
-}
+}*/
   
