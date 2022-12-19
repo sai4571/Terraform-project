c487fc33b529a1df6feefae73c020e776cc21286
sai4571 <110476592+sai4571@users.noreply.github.com>
2022-12-19 16:34:22 +0530

    Update IAMuser.tf

--- IAMuser.tf
+++ IAMuser.tf
@@ -1,6 +1,6 @@
-resource "aws_iam_user" "IAMUser"{
+/*resource "aws_iam_user" "IAMUser"{
     //name = "saicherry"
      count = length(var.iam_users)
       name = var.iam_users[count.index]  
          
-  }
+  }*/
