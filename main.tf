8b3e78ceb7daae480cdd3aafbc1440aa9f491df5
sai4571 <110476592+sai4571@users.noreply.github.com>
2022-12-19 16:36:33 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -2,8 +2,8 @@ resource "aws_instance" "AWSEC2Instance"{
     ami = "ami-0f9d9a251c1a44858"
     instance_type = "t2.micro" 
     key_name = "awstest"
-    security_groups = ["${aws_security_group.forapache.name}"]
-    user_data = "${file("install_apache.sh")}"
+    /*security_groups = ["${aws_security_group.forapache.name}"]
+    user_data = "${file("install_apache.sh")}"*/
     
     tags = {
         Name = "RedHat Server by Terraform - for apache"
