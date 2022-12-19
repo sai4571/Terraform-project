b681defbd3fa838442453e82bd51b3a6e7f36dfc
sai4571 <110476592+sai4571@users.noreply.github.com>
2022-12-19 21:10:11 +0530

    updated apache file

--- install_apache.sh
+++ install_apache.sh
@@ -1,6 +1,6 @@
-/*#!/bin/bash
+#!/bin/bash
 sudo yum update -y
 sudo yum install -y httpd
 echo '<h1>Terraform Sample Project</h1>' > /var/www/html/index.html
 sudo systemctl start httpd
-sudo systemctl enable httpd*/
+sudo systemctl enable httpd
